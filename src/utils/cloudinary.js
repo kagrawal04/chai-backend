f475a6d7a5147cee9de741082c2fe2fd3f6ef01d
Kaushal Agrawal <cu.16bcs2632@gmail.com>
2024-07-21 20:50:52 +0530

    add cloudinary, multer middleware

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,36 @@
+import {v2 as cloudinary} from "cloudinary"
+import { response } from "express";
+import fs from "fs"
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary= async(localFilePath)=>{
+    try {
+        if (!localFilePath) return null
+        // upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        // file has been uploaded successfully
+        console.log("File has benn uploaded successfully on cloudinary", 
+        response.url);
+        return response
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath)// remove locally saved temporary file as the uploaded operation got failed
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
+
+/*cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg", 
+    {
+        public_id: "olympic_flag"
+    },
+    function(error, result){console.log(result);}    
+);*/
